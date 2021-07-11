26aebcf793d2925536e7a93cfeb9fff5c817b680
Felipe Cunha <felipe@cosmobots.io>
2021-07-11 11:41:57 -0300

    ajusta deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: yarn run ts-node ./src/server.ts
--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,6 @@ app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333!');
+app.listen(process.env.PORT || 3333, () => {
+  console.log('Server started!');
 });
